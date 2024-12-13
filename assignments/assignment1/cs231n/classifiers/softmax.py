04f44525b1acd54495c1a06c0c0dca4625065128
ChenClay <88125728+Iamnotphage@users.noreply.github.com>
2024-12-13 15:13:55 +0800

    feat: finish the softmax.ipynb

--- assignments/assignment1/cs231n/classifiers/softmax.py
+++ assignments/assignment1/cs231n/classifiers/softmax.py
@@ -34,8 +34,33 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
 
+    for i in range(num_train):
+      scores = X[i].dot(W)
+      # 对于计算loss，因为要exp，同时减去最大值计算值不变，但是能避免溢出或者数值的不稳定
+      scores = scores - max(scores)
+      
+      exp_sum = np.sum(np.exp(scores))
+
+      # loss_i = -log(exp(s_yi)/sum(s_j)) = -s_yi + log(sum(s_j))
+      # 可以避免一次log产生的误差
+      loss += -scores[y[i]] + np.log(exp_sum)
+
+      # 求导数，这里推导公式即可
+      for j in range(num_classes):
+        temp = np.exp(scores[j]) / exp_sum
+        if j == y[i]:
+          dW[:,j] += X[i].T * (temp - 1)
+        else:
+          dW[:,j] += X[i].T * temp
+          
+    loss /= num_train
+    loss += reg * np.sum(W * W)
+
+    dW /= num_train
+    dW += 2 * reg * W
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
@@ -59,8 +84,23 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    pass
+    num_classes = W.shape[1]
+    num_train = X.shape[0]
+    # 点乘得到一个(N, C)的得分矩阵，一行代表第i个图像的得分情况
+    scores = X.dot(W)
+    scores = scores - np.max(scores, axis = 1).reshape(-1,1) # shift消减误差积累
+    
+    # 计算出所有 e^s_ij / sum(e^s_ij)
+    portion = np.exp(scores) / np.sum(np.exp(scores), axis = 1).reshape(-1,1)
+    # 挑出 y_i
+    loss = -np.sum(np.log(portion[range(num_train), list(y)]))
+    loss /= num_train
+    loss += reg * np.sum(W * W)
 
+    portion[range(num_train), list(y)] += -1
+    dW = (X.T).dot(portion)
+    dW /= num_train
+    dW += 2 * reg * W
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     return loss, dW
